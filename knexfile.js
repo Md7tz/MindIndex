121ca543337614c8a61b0d65e39511cf2a55d33c
Medhat Usama <medhatusama@gmail.com>
2023-04-15 22:08:55 +0800

    feat: Added Objection js base model

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: process.env.DB_CONNECTION,
+    useNullAsDefault: true,
+    connection: {
+      host: process.env.DB_HOST || 'host.docker.internal',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: '../migrations/',
+      tableName: 'knex_migrations'
+    }
+  },
+
+  docker: {
+    client: process.env.DB_CONNECTION,
+    useNullAsDefault: true,
+    connection: {
+      host: process.env.DB_HOST || 'db',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'toor',
+      database: process.env.DB_NAME || 'database',
+      port: process.env.DB_PORT || 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: '../migrations/',
+
+      tableName: 'knex_migrations'
+    }
+  },
+};
--- models/index.mjs
+++ models/index.mjs
@@ -1,1 +1,31 @@
-export default class Model { }
+import { Model } from 'objection';
+import visibilityPlugin from 'objection-visibility';
+import softDelete from 'objection-soft-delete';
+import moment from 'moment';
+
+const visibility = visibilityPlugin.default;
+
+export default class BaseModel extends visibility(softDelete(Model)) {
+    static get softDelete() {
+        return true;
+    }
+
+    $beforeInsert() {
+        this.created_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+    }
+
+    $beforeUpdate() {
+        this.updated_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+    }
+}
+
+BaseModel.tableName = 'base_model';
+BaseModel.jsonSchema = {
+    type: 'object',
+    properties: {
+        id: { type: 'integer' },
+        created_at: { type: 'string', format: 'date-time' },
+        updated_at: { type: ['string', 'null'], format: 'date-time' },
+        deleted_at: { type: ['string', 'null'], format: 'date-time' },
+    },
+};
