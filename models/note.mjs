0806dcaa45e1efc90e0e9a89a561246fe3359a75
Medhat Usama <medhatusama@gmail.com>
2023-04-17 04:47:40 +0800

    chore: added jsdoc for Note model

--- models/note.mjs
+++ models/note.mjs
@@ -1,19 +1,34 @@
 import BaseModel from './index.mjs';
+import { MAX_STRING_LENGTH, MAX_TEXT_LENGTH } from '../config/constants.mjs';
 
+/**
+ * @class Note
+ * @extends BaseModel
+ * @memberof Model
+ * @description Represents a note in the database.
+ *
+ * @property {Number} id               - The incremental ID of the note.
+ * @property {String} title            - The title of the note.
+ * @property {String|Text} body             - The body of the note.
+ * @property {Date} created_at         - The timestamp of when the note was created.
+ * @property {Date|null} updated_at    - The timestamp of when the note was last updated, or null if it has not been updated.
+ * @property {Date|null} deleted_at    - The timestamp of when the note was deleted, or null if it has not been deleted.
+ */
 export default class Note extends BaseModel {
-    
-}
+  // Set the database table name for Note objects
+  static tableName = 'notes';
 
-Note.tableName = 'notes';
-Note.jsonSchema = {
+  // Define the JSON schema for Note objects
+  static jsonSchema = {
     type: 'object',
     required: ['title', 'body'],
     properties: {
-        id: { type: 'integer' },
-        title: { type: 'string', maxLength: 255},
-        body: {type: 'string', maxLength: 65535},
-        created_at: { type: 'string', format: 'date-time' },
-        updated_at: { type: ['string', 'null'], format: 'date-time' },
-        deleted_at: { type: ['string', 'null'], format: 'date-time' },
+      id: { type: 'integer' },
+      title: { type: 'string', maxLength: MAX_STRING_LENGTH },
+      body: { type: 'text', maxLength: MAX_TEXT_LENGTH },
+      created_at: { type: 'string', format: 'date-time' },
+      updated_at: { type: ['string', 'null'], format: 'date-time' },
+      deleted_at: { type: ['string', 'null'], format: 'date-time' },
     },
-};
+  };
+}
