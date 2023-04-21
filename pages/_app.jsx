f741ee0449f095876a0a81eafcdbeb10df9ecba0
Medhat Usama <medhatusama@gmail.com>
2023-04-21 15:28:44 +0800

    refactor: move styles && toast container to main app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
+import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
+        <ToastContainer />
       </Layout>
     </>
   );
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.css";
+import { toast } from "react-toastify";
+
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -65,7 +64,7 @@ export default function Test() {
             fixedWidth
           />
         </button>
-        <ToastContainer />
+        
       </div>
     </>
   );
