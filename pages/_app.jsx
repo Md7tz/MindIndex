af31a6baeb6e39fa4ecba68fe7d99461b01b5cb6
Ayoub Ennabili <93245603+Kayzenn4@users.noreply.github.com>
2023-04-21 16:16:34 +0800

    fix: update bootstrap script in head tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
-import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <Layout>
         <Component {...pageProps} />
         <ToastContainer />
