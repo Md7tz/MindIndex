f0b02cd4e13ac020a84f1f3d4b91c479a825a980
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-04-24 17:14:19 +0800

    feat: added the initial Navbar design

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,82 @@
 import React from "react";
-
+import "../styles/Navbar.module.css";
 export default function NavBar() {
   return (
-    <div></div>
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white border-end px-4" href="/">
+          MindIndex
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => {
+            const navbar = document.getElementById("navbarSupportedContent");
+            navbar.classList.toggle("show");
+          }}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-white" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+           
+            <li className="nav-item dropdown ">
+              <a
+                className="nav-link dropdown-toggle text-white "
+                href="#"
+                role="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  const dropdownMenu =
+                    document.getElementById("navbarDropdown");
+                  dropdownMenu.classList.toggle("show");
+                }}
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }
