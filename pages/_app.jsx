1db7cb6ca174656b6993112134dafa048913f224
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-04-24 16:35:34 +0800

    feat: added the cdn links

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Layout from "../components/layout";
 import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Script from "next/script";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -9,15 +10,24 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-          crossorigin="anonymous"
-        ></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossOrigin="anonymous"
+        />
       </Head>
+
       <Layout>
         <Component {...pageProps} />
         <ToastContainer />
+
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossOrigin="anonymous"
+        />
       </Layout>
     </>
   );
