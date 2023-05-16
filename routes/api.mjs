425cb1f7554027964705174d00dd5ebf68409c8f
Medhat Usama <medhatusama@gmail.com>
2023-05-17 03:28:56 +0800

    feat: flashcard collections controller

--- routes/api.mjs
+++ routes/api.mjs
@@ -1,13 +1,18 @@
 import express from 'express';
 
 // Controllers
-import Controller from '../controllers/index.mjs';
 import Collection from '../controllers/Collection.mjs';
 
 // Middleware
 import ErrorHandler from '../middlewares/ErrorHandler.mjs';
 
 const router = express.Router();
+
+router.get('/collections', Collection.getAllCollections);
+router.get('/collections/:id', Collection.getCollectionById);
+router.post('/collections', Collection.createCollection);
+router.put('/collections/:id', Collection.updateCollection);
+router.delete('/collections/:id', Collection.deleteCollection);
 
 router.use(ErrorHandler);
 
