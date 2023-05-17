a4ee36dfeacde83a2667e77826e41c827a7257a6
Ayoub Ennabili <93245603+Kayzenn4@users.noreply.github.com>
2023-05-18 02:37:20 +0800

    feat: added the api endpoints for user and profile

--- routes/api.mjs
+++ routes/api.mjs
@@ -1,16 +1,26 @@
 import express from "express";
 
 // Controllers
-import Collection from '../controllers/Collection.mjs';
-import Flashcard from '../controllers/Flashcard.mjs';
+import User from "../controllers/User.mjs";
+import Profile from "../controllers/Profile.mjs";
+import Collection from "../controllers/Collection.mjs";
+import Flashcard from "../controllers/Flashcard.mjs";
 import Note from "../controllers/Note.mjs";
 
-
 // Middleware
-import ErrorHandler from '../middlewares/ErrorHandler.mjs';
+import ErrorHandler from "../middlewares/ErrorHandler.mjs";
 
 const router = express.Router();
 
+// User Routes
+router.get("/users", User.getAllUsers);
+router.get("/users/:id", User.getUserById);
+router.post("/users", User.createUser);
+router.put("/users/:id", User.updateUser);
+router.delete("/users/:id", User.deleteUser);
+
+// Profile Routes
+router.put("/profile/:id", Profile.updateProfile);
 
 // Notes Routes
 router.get("/notes", Note.getAllNotes);
@@ -20,17 +30,16 @@ router.put("/notes/:id", Note.updateNote);
 router.delete("/notes/:id", Note.deleteNote);
 
 // Collection Routes
-router.get('/collections', Collection.getAllCollections);
-router.get('/collections/:id', Collection.getCollectionById);
-router.post('/collections', Collection.createCollection);
-router.put('/collections/:id', Collection.updateCollection);
-router.delete('/collections/:id', Collection.deleteCollection);
+router.get("/collections", Collection.getAllCollections);
+router.get("/collections/:id", Collection.getCollectionById);
+router.post("/collections", Collection.createCollection);
+router.put("/collections/:id", Collection.updateCollection);
+router.delete("/collections/:id", Collection.deleteCollection);
 
 // Flashcards routes
-router.put('/flashcards/:id', Flashcard.updateFlashcard);
-router.delete('/flashcards/:id', Flashcard.deleteFlashcard );
+router.put("/flashcards/:id", Flashcard.updateFlashcard);
+router.delete("/flashcards/:id", Flashcard.deleteFlashcard);
 
 router.use(ErrorHandler);
 
 export default router;
-
