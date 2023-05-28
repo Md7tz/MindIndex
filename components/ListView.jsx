2afccd869dd091960332bc9d35951674f604bca3
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-05-28 13:19:26 +0800

    feat: added the body and descriptoin to listing view

--- components/ListView.jsx
+++ components/ListView.jsx
@@ -3,33 +3,31 @@ import styles from "../styles/ListView.module.css";
 
 const ListView = ({ collections, notes }) => {
   return (
-    <div className={`m-2 ${styles["list-view-container"]}` }>
+    <div className={`m-2 ${styles["list-view-container"]}`}>
       <div className={styles["list-view"]}>
         <table className={styles["list-view-table"]}>
           <thead>
             <tr>
-              <th>Collections</th>
+              <th>Item</th>
+              <th>Content</th> {/* New column for collection description */}
             </tr>
           </thead>
           <tbody>
             {collections.map((collection) => (
               <tr key={collection.id}>
                 <td>{collection.name}</td>
+                <td>{collection.description}</td> {/* Display collection description */}
               </tr>
             ))}
-            {/* {console.log(`collectoins: ${collections}` )} */}
           </tbody>
         </table>
         <table className={styles["list-view-table"]}>
-          <thead>
-            <tr>
-              <th>Notes</th>
-            </tr>
-          </thead>
+         
           <tbody>
             {notes.map((note) => (
               <tr key={note.id}>
                 <td>{note.title}</td>
+                <td>{note.body}</td> {/* Display note body */}
               </tr>
             ))}
           </tbody>
