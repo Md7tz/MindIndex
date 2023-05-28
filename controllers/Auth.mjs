2709870825927716de7db6cc103f994abcecc91c
Medhat Usama <medhatusama@gmail.com>
2023-05-29 00:22:55 +0800

    refactor: remove auth0 integration

--- controllers/Auth.mjs
+++ controllers/Auth.mjs
@@ -1,82 +1,136 @@
+import Validator from "validatorjs";
+import { HTTP } from "../config/constants.mjs";
+import { transaction } from "objection";
+import User from "../models/User.mjs";
+import Profile from "../models/Profile.mjs";
+
 export default class Auth {
 
-    static async register(req, res, next) {
-        try {
-            res.oidc.login({
-                returnTo: "/",
-                authorizationParams: {
-                  screen_hint: 'signup',
-                },
-              });
-        } catch (error) {
-            next(error);
-        }
+  static async renderRegister(req, res, next) {
+    try {
+      res.oidc.login({
+        returnTo: "/",
+        authorizationParams: {
+          screen_hint: 'signup',
+        },
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async login(req, res, next) {
-        try {
-            res.oidc.login({
-                authorizationParams: {
-                  screen_hint: 'login',
-                },
-              });
-        } catch (error) {
-            next(error);
-        }
-    }
+  static async register(req, res, next) {
+    try {
+      // const validation = new Validator(req.body, {
+      //   user: "required",
+      // });
 
-    static async logout(req, res, next) {
-        try {
-            res.oidc.logout({
-                returnTo: "/",
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
+      // if (validation.fails()) {
+      //   return res.status(HTTP.BAD_REQUEST).json({
+      //     message: "Validation failed.",
+      //     errors: validation.errors.all(),
+      //   });
+      // }
 
-    static async callback(req, res, next) {
-        try {
-            res.oidc.authenticated({
-                returnTo: "/",
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
+      const { user: { email, username, user_metadata } } = req.body;
 
-    static async refresh(req, res, next) {
-        try {
-            res.oidc.refreshTokens().then((result) => {
-                res.json(JSON.stringify(result));
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
+      // check if user exist or not
+      const exists = await User.query().where("email", email).orWhere("username", username).first();
+      if(exists) return;
 
-    static async userinfo(req, res, next) {
-        try {
-            res.send(JSON.stringify(req.oidc.user));
-        } catch (error) {
-            next(error);
-        }
-    }
+      console.log(req.body);
+      await transaction(User.knex(), async (trx) => {
+        const user = await User.query(trx).insert({
+          username,
+          email
+        });
 
-    static async token(req, res, next) {
-        try {
-            res.send(JSON.stringify(req.oidc.accessToken));
-        } catch (error) {
-            next(error);
-        }
-    }
+        const profile = await Profile.query(trx).insert({
+          fullname: user_metadata.fullname || "",
+          // You can add other profile properties here based on your requirements
+        });
 
-    static async profile(req, res, next) {
-        try {
-            res.json(await req.oidc.fetchUserInfo())
-        } catch (error) {
-            next(error);
-        }
+        await profile.$relatedQuery("user", trx).relate(user.id);
+
+        req.user = user;
+        req.user.profile = profile;
+        // return res.status(HTTP.CREATED).json({
+        //   message: "User created.",
+        //   user,
+        //   profile,
+        // });
+      });
+    } catch (error) {
+      console.error(error);
+      next(error);
     }
+  }
+
+  static async renderLogin(req, res, next) {
+    try {
+      res.oidc.login({
+        authorizationParams: {
+          screen_hint: 'login',
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async redirectLogout(req, res, next) {
+    try {
+      res.oidc.logout({
+        returnTo: "/",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async callback(req, res, next) {
+    try {
+      console.log(req.oidc);
+      next();
+      const callbackURI = process.env.AUTH0_CALLBACK_URI || "http://localhost:3000/callback";
+      return res.redirect(callbackURI);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async refresh(req, res, next) {
+    try {
+      res.oidc.refreshTokens().then((result) => {
+        res.json(JSON.stringify(result));
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async userinfo(req, res, next) {
+    try {
+      res.send(JSON.stringify(req.oidc.user));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async token(req, res, next) {
+    try {
+      res.send(JSON.stringify(req.oidc.accessToken));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async profile(req, res, next) {
+    try {
+      res.json(await req.oidc.fetchUserInfo())
+    } catch (error) {
+      next(error);
+    }
+  }
 
 }
--- routes/api.mjs
+++ routes/api.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import openid from "express-openid-connect";
 
 // Controllers
 import User from "../controllers/User.mjs";
@@ -12,25 +11,8 @@ import Auth from "../controllers/Auth.mjs";
 // Middleware
 import ErrorHandler from "../middlewares/ErrorHandler.mjs";
 
-// Config
-import config from "../config/auth0.mjs";
-
 const router = express.Router();
-const { requiresAuth: authenicate, auth } = openid;
 
-// Auth0 Routes
-// are: /login, /logout, /callback, /refresh, /userinfo, 
-router.use(auth(config));
-
-// Auth: override default auth0 routes
-router.get("/register", Auth.register);
-router.get("/login", Auth.login);
-router.get("/logout", authenicate(), Auth.logout);
-router.get("/refresh", authenicate(), Auth.refresh); // refresh token
-router.get("/token", authenicate(), Auth.token); // access token
-router.get("/userinfo", authenicate(), Auth.userinfo); // user info
-router.get("/callback", authenicate(), Auth.callback); // callback url after login
-router.get("/profile", authenicate(), Auth.profile); // initial profile page
 
 // User Routes
 router.get("/users", User.getAllUsers);
@@ -40,27 +22,27 @@ router.put("/users/:id", User.updateUser);
 router.delete("/users/:id", User.deleteUser);
 
 // Profile Routes
-router.get("users/:id/profiles", authenicate(), Profile.getProfileByUserId);
-// router.post("users/:id/profiles", authenicate(),Profile.createProfile);
-router.put("users/:id/profiles", authenicate(), Profile.updateProfile);
+router.get("users/:id/profiles", Profile.getProfileByUserId);
+// router.post("users/:id/profiles",Profile.createProfile);
+router.put("users/:id/profiles", Profile.updateProfile);
 
 // Notes Routes
-router.get("/notes", authenicate(), Note.getAllNotes);
-router.get("/notes/:id", authenicate(), Note.getNoteById);
-router.post("/notes", authenicate(), Note.createNote);
-router.put("/notes/:id", authenicate(), Note.updateNote);
-router.delete("/notes/:id", authenicate(), Note.deleteNote);
+router.get("/notes", Note.getAllNotes);
+router.get("/notes/:id", Note.getNoteById);
+router.post("/notes", Note.createNote);
+router.put("/notes/:id", Note.updateNote);
+router.delete("/notes/:id", Note.deleteNote);
 
 // Collection Routes
-router.get("/collections", authenicate(), Collection.getAllCollections);
-router.get("/collections/:id", authenicate(), Collection.getCollectionById);
-router.post("/collections", authenicate(), Collection.createCollection);
-router.put("/collections/:id", authenicate(), Collection.updateCollection);
-router.delete("/collections/:id", authenicate(), Collection.deleteCollection);
+router.get("/collections", Collection.getAllCollections);
+router.get("/collections/:id", Collection.getCollectionById);
+router.post("/collections", Collection.createCollection);
+router.put("/collections/:id", Collection.updateCollection);
+router.delete("/collections/:id", Collection.deleteCollection);
 
 // Flashcards routes
-router.put("/flashcards/:id", authenicate(), Flashcard.updateFlashcard);
-router.delete("/flashcards/:id", authenicate(), Flashcard.deleteFlashcard);
+router.put("/flashcards/:id", Flashcard.updateFlashcard);
+router.delete("/flashcards/:id", Flashcard.deleteFlashcard);
 
 router.use(ErrorHandler);
 
