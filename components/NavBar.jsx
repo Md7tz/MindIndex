4da0a337d4f2c4ca45e9014088ab108d22c608ad
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-05-28 11:38:50 +0800

    refactor: seprated the search-bar from the nav-bar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,6 +5,7 @@ import Logo from "./imgs/Logo.jpg";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 import axios from "axios";
+import SearchBar from "./SearchBar";
 
 export default function NavBar() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -179,53 +180,13 @@ export default function NavBar() {
             </li>
           </ul>
 
-          <form
-            className="d-flex input-group w-auto ms-lg-3 my-3 my-lg-0"
-            onSubmit={handleSubmit}
-          >
-            <input
-              type="search"
-              name="searchQuery"
-              value={searchQuery}
-              className={`form-control ${styles["search-box"]}`}
-              onChange={onChange}
-              placeholder="Search MindIndex"
-              aria-label="Search MindIndex"
-            />
-            <button
-              className="btn btn-dark"
-              type="submit"
-              data-mdb-ripple-color="dark"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-search"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
-              </svg>
-            </button>
-            {/* Display filtered collections as a dropdown list */}
-            <select>
-              {filteredCollections.map((collection) => (
-                <option key={collection.id} value={collection.id}>
-                  {collection.name}
-                </option>
-              ))}
-            </select>
-
-            {/* Display filtered notes as a dropdown list */}
-            <select>
-              {filteredNotes.map((note) => (
-                <option key={note.id} value={note.id}>
-                  {note.title}
-                </option>
-              ))}
-            </select>
-          </form>
+          <SearchBar
+            handleSubmit={handleSubmit}
+            searchQuery={searchQuery}
+            onChange={onChange}
+            filteredCollections={filteredCollections}
+            filteredNotes={filteredNotes}
+          />
         </div>
       </div>
       <LoginForm />
