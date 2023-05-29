b9dbfb5004b3930b926625a319a87c39c85ed029
Medhat Usama <medhatusama@gmail.com>
2023-05-29 23:34:22 +0800

    feat: create token helper

---
+++ config/token.mjs
@@ -0,0 +1,21 @@
+import moment from "moment";
+import jwt from "jwt-simple";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default class Token {
+    static generate(user_id, ttl = 86400, nonce = null) {
+
+        const payload = {
+            sub: user_id,
+            iss: process.env.APP_URL,
+            ttl: ttl,
+            iat: moment().unix(),
+            exp: iat + ttl,
+            nonce: nonce
+        };
+
+        return jwt.encode(payload, process.env.JWT_SECRET);
+    }
+}
