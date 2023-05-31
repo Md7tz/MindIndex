f2bb498a2e5dd80c698b64cfe95b74711072b26f
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-05-31 16:41:25 +0800

    refactor: reduce the amount of react states

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,81 +1,68 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ListView from "./ListView";
 import Fuse from "fuse.js";
+import ListView from "./ListView";
 import styles from "../styles/Navbar.module.css";
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [collections, setCollections] = useState([]);
-  const [notes, setNotes] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchCollections = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get("/api/collections");
-        setCollections(response.data["collections"]);
+        const [collectionsResponse, notesResponse] = await Promise.all([
+          axios.get("/api/collections"),
+          axios.get("/api/notes"),
+        ]);
+
+        const collections = collectionsResponse.data.collections.map(
+          (collection) => ({
+            ...collection,
+            merged_id: `collection_${collection.id}`,
+            type: "collection",
+          })
+        );
+
+        const notes = notesResponse.data.notes.map((note) => ({
+          ...note,
+          merged_id: `note_${note.id}`,
+          type: "note",
+        }));
+
+        setData([...collections, ...notes]);
       } catch (error) {
         console.log(error);
       }
     };
 
-    const fetchNotes = async () => {
-      try {
-        const response = await axios.get("/api/notes");
-        setNotes(response.data["notes"]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchCollections();
-    fetchNotes();
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    const filterData = () => {
-      const options = {
+  const filteredData = searchQuery
+    ? new Fuse(data, {
         keys: ["title", "body", "description", "name"],
         includeScore: true,
         threshold: 0.4,
-      };
+      })
+        .search(searchQuery)
+        .map((result) => result.item)
+    : [];
 
-      const collectionsWithId = collections.map((collection) => ({
-        ...collection,
-        merged_id: `collection_${collection.id}`,
-        type: "collection",
-      }));
-      const notesWithId = notes.map((note) => ({
-        ...note,
-        merged_id: `note_${note.id}`,
-        type: "note",
-      }));
-
-      const fuseData = new Fuse([...collectionsWithId, ...notesWithId], options);
-      const filteredData = fuseData.search(searchQuery);
-
-      setFilteredData(filteredData.map((result) => result.item));
-    };
-
-    filterData();
-  }, [searchQuery, collections, notes]);
-
-  const onChange = async (event) => {
+  const onChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
   };
 
-  const handleSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    console.log("HI");
+    console.log("Submit");
   };
 
   return (
     <form
       className="d-flex input-group w-auto ms-lg-3 my-3 my-lg-0"
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
     >
       <input
         type="search"
@@ -85,14 +72,8 @@ const SearchBar = () => {
         onChange={onChange}
         placeholder="Search MindIndex"
         aria-label="Search MindIndex"
-        onFocus={() => setIsSearchFocused(true)}
-        onBlur={() => setIsSearchFocused(false)}
       />
-      <button
-        className="btn btn-dark"
-        type="submit"
-        data-mdb-ripple-color="dark"
-      >
+      <button className="btn btn-dark" type="submit" data-mdb-ripple-color="dark">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -101,12 +82,10 @@ const SearchBar = () => {
           className="bi bi-search"
           viewBox="0 0 16 16"
         >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
         </svg>
       </button>
-      {isSearchFocused && searchQuery.length > 0 && (
-        <ListView data={filteredData} />
-      )}
+      {searchQuery.length > 0 && <ListView data={filteredData} />}
     </form>
   );
 };
