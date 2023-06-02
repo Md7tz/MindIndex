422d07655b834afd3ae538439212eab93749e687
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-06-03 06:33:32 +0800

    feat: finalized the search page

--- pages/search.jsx
+++ pages/search.jsx
@@ -18,17 +18,24 @@ const SearchPage = () => {
   const filteredResults = filteredData(searchQuery, data);
   return (
     <div>
+      <header className="py-3 text-black ">
+        <div className="container-fluid">
+          <h1 className="">Results for "{searchQuery}"</h1>
+        </div>
+      </header>
+
       <div className="container-fluid">
-        <div className="row">
+        <div className="row row-cols-1 row-cols-md-3 g-4 ">
           {filteredResults.map((result) => (
-            <div className="col-md-4 mb-4 pt-4" key={result.merged_id}>
-              <div className="card">
-                <div className="card-body">
+            <div className="col" key={result.merged_id}>
+              <div className={`card ${result.type === "collection" ? "bg-info" : "bg-dark"} text-white h-100`}>
+                <div className="card-body d-flex flex-column">
                   <h5 className="card-title">{result.title || result.name}</h5>
-                  <p className="card-text">
-                    {result.description || result.body}
-                  </p>
-                    <a href="#" class="card-link">link to the note/collection view</a>
+                  <p className="card-text">{result.description || result.body}</p>
+                  <div className="mt-auto d-flex justify-content-end">
+                    <a href="#" className="btn btn-primary ">Preview</a>
+                    {/* link-to-either-collection-or-note use, result.type/result.id */}
+                  </div>
                 </div>
               </div>
             </div>
