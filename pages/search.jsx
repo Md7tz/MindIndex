8593ceb796d4517317b48bedb1fb89701bc3af96
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-06-03 05:56:12 +0800

    feat: added links to views

--- pages/search.jsx
+++ pages/search.jsx
@@ -18,17 +18,22 @@ const SearchPage = () => {
   const filteredResults = filteredData(searchQuery, data);
   return (
     <div>
-      <div className="row">
-        {filteredResults.map((result) => (
-          <div className="col-md-4 mb-4" key={result.merged_id}>
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{result.title || result.name}</h5>
-                <p className="card-text">{result.description || result.body}</p>
+      <div className="container-fluid">
+        <div className="row">
+          {filteredResults.map((result) => (
+            <div className="col-md-4 mb-4 pt-4" key={result.merged_id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{result.title || result.name}</h5>
+                  <p className="card-text">
+                    {result.description || result.body}
+                  </p>
+                    <a href="#" class="card-link">link to the note/collection view</a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
