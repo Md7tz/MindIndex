bd3d9b3ebb2a499699416ff276623c3ec48d38c4
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-06-03 05:45:59 +0800

    feat: added search page && fetch and filterd the data

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { fetchData, filteredData } from "/utils/search";
 
 const SearchPage = () => {
-  
-  return <div>Search</div>;
+  const [data, setData] = useState([]);
+
+  const router = useRouter();
+  const { searchQuery } = router.query;
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      setData(await fetchData());
+    };
+
+    fetchDataAsync();
+  }, []);
+
+  const filteredResults = filteredData(searchQuery, data);
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <p>Search Query: {}</p>
+    </div>
+  );
 };
 
 export default SearchPage;
