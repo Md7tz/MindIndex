5dcd5a5cfd1f42d411d8b0cad4183709100ee3bc
Ayoub Ennabili <93245603+Kayzenn4@users.noreply.github.com>
2023-06-20 19:35:12 +0800

    feat: stripe config & server side checkout

--- app.mjs
+++ app.mjs
@@ -1,25 +1,28 @@
-import express from 'express';
-import next from 'next';
-import cors from 'cors';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import multer from 'multer';
-import knex from 'knex';
-import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
-import { Model } from 'objection';
+import express from "express";
+import next from "next";
+import cors from "cors";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import knex from "knex";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import { Model } from "objection";
 
-import _dbConfig from './knexfile.js';
-import swaggerDocs from './config/swagger.mjs';
+import _dbConfig from "./knexfile.js";
+import swaggerDocs from "./config/swagger.mjs";
 
-import api from './routes/api.mjs';
-import Passport from './middlewares/Passport.mjs';
+import api from "./routes/api.mjs";
+import Passport from "./middlewares/Passport.mjs";
 
-import dotenv from 'dotenv';
+import stripe from "stripe";
+const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
+
+import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const upload = multer();
@@ -27,17 +30,17 @@ const upload = multer();
 let dbConfig = null;
 
 switch (process.env.NODE_ENV) {
-    case 'production':
-        dbConfig = _dbConfig.production;
-        break;
-    case 'development':
-        dbConfig = _dbConfig.development;
-        break;
-    case 'test':
-        dbConfig = _dbConfig.docker;
-        break;
-    default:
-        throw new Error(`Unknown environment: ${NODE_ENV}`);
+  case 'production':
+    dbConfig = _dbConfig.production;
+    break;
+  case 'development':
+    dbConfig = _dbConfig.development;
+    break;
+  case 'test':
+    dbConfig = _dbConfig.docker;
+    break;
+  default:
+    throw new Error(`Unknown environment: ${NODE_ENV}`);
 }
 
 const db = knex(dbConfig);
@@ -50,25 +53,45 @@ server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser());
 server.use(compression());
 server.use(upload.any());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 
 // Serve static files from the 'public' directory
-server.use(express.static('public'));
+server.use(express.static("public"));
 
 // Routers
 server.use(`/api`, api);
+server.post("/checkout", async (req, res) => {
+  try {
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items: [
+        {
+          price: "price_1NKw9nEYJpcZy5dC7dAqMFBL",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      payment_method_types: ["card"],
+      success_url: `${req.headers.origin}/?success=true`,
+      cancel_url: `${req.headers.origin}/?canceled=true`,
+    });
+    res.json({ url: session.url });
+    // res.redirect(303, session.url);
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
+  }
+});
 
 // Serve Swagger UI at /docs
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Handle all other routes
-server.all('*', (req, res) => {
-    return handle(req, res);
+server.all("*", (req, res) => {
+  return handle(req, res);
 });
 
 // Start server
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 // Bind the database instance to the Objection.js Model
