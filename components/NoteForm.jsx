53f085749218121cd20ff291f42f02fd0b5cdcdf
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2023-06-28 03:15:45 +0800

    SCIEN-62 <feat: Enable the note form to be used as update

--- components/NoteForm.jsx
+++ components/NoteForm.jsx
@@ -2,18 +2,70 @@ import styles from "./styles/NoteForm.module.css";
 import React, { useEffect } from "react";
 
 const NoteForm = ({ mode, index }) => {
+  const [formMode, setFormMode] = React.useState(mode);
   useEffect(() => {
-    console.log(`mode: ${mode}, index: ${index}`);
-  }, [mode, index]);
+    console.log(`mode: ${formMode}, index: ${index}`);
+  }, [formMode, index]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
   };
 
+  const renderViewMode = () => {
+    return (
+      <div className={styles["view-mode-container"]}>
+        <h6 className={styles["view-mode-title"]}>Wanderlust Chronicles</h6>
+        <p className={styles["view-mode-text"]}>
+          Venturing into the realm of travel, discovering new destinations and experiences.
+        </p>
+        <button
+          type="button"
+          className={`btn btn-primary ml-4 ${styles["note-form-button"]} text-center`}
+          onClick={() => {
+            setFormMode("edit");
+          }}
+        >
+          Update Note
+        </button>
+      </div>
+    );
+  };
+
+  const renderEditMode = () => {
+    return (
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className={`form-label ${styles["note-form-label"]}`}>
+            Title
+          </label>
+          <input type="text" className="form-control note-form-control" id="title" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="body" className={`form-label ${styles["note-form-label"]}`}>
+            Body
+          </label>
+          <textarea
+            className={`form-control ${styles["note-form-control"]}`}
+            id="body"
+            rows="3"
+          ></textarea>
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className={`btn btn-primary ml-4 ${styles["note-form-button"]} text-center`}
+          >
+            {formMode === "create" ? "Create Note" : "Update Note"}
+          </button>
+        </div>
+      </form>
+    );
+  };
+
   return (
     <div
       className="modal fade note-modal"
-      id={`${mode}${index ? index : ""}`}
+      id={`${formMode}${index ? index : ""}`}
       data-bs-backdrop="static"
       data-bs-keyboard="false"
       tabIndex="-1"
@@ -23,11 +75,7 @@ const NoteForm = ({ mode, index }) => {
         <div className={`modal-content ${styles["note-modal-content"]}`}>
           <div className={`modal-header ${styles["note-modal-header"]}`}>
             <h5 className="modal-title w-100 text-center" id="noteModalLabel">
-              {mode === "view"
-                ? "View Note"
-                : mode === "create"
-                ? "Create Note"
-                : "Edit Note"}
+              {formMode === "view" ? "View Note" : formMode === "create" ? "Create Note" : "Edit Note"}
             </h5>
             <button
               type="button"
@@ -37,49 +85,7 @@ const NoteForm = ({ mode, index }) => {
             ></button>
           </div>
           <div className={`modal-body ${styles["note-modal-body"]}`}>
-            {mode === "view" ? (
-              <div className={styles["view-mode-container"]}>
-                <h6 className={styles["view-mode-title"]}>Wanderlust Chronicles</h6>
-                <p className={styles["view-mode-text"]}>Venturing into the realm of travel, discovering new destinations and experiences.</p>
-                <a
-                  type="submit"
-                  className={`btn btn-primary ml-4 ${styles["note-form-button"]} text-center`}
-                >
-                  Update Note
-                </a>
-              </div>
-            ) : (
-              <form onSubmit={onSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="title" className="form-label note-form-label">
-                    Title
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control note-form-control"
-                    id="title"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="body" className="form-label note-form-label">
-                    Body
-                  </label>
-                  <textarea
-                    className={`form-control ${styles["note-form-control"]}`}
-                    id="body"
-                    rows="3"
-                  ></textarea>
-                </div>
-                <div className="text-center">
-                  <button
-                    type="submit"
-                    className={`btn btn-primary ml-4 ${styles["note-form-button"]} text-center`}
-                  >
-                    ${mode == "create" ? "Create Note" : "Update Note"}
-                  </button>
-                </div>
-              </form>
-            )}
+            {formMode === "view" ? renderViewMode() : renderEditMode()}
           </div>
           <div className="modal-footer">
             <button
