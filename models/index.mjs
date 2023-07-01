f243532483393a5d961835319c76262e6f39e769
Medhat Usama <medhatusama@gmail.com>
2023-07-02 03:19:50 +0800

    feat: base model modifier

--- models/index.mjs
+++ models/index.mjs
@@ -1,4 +1,4 @@
-import { Model, QueryBuilder } from 'objection';
+import { Model, QueryBuilder, raw } from 'objection';
 import visibilityPlugin from 'objection-visibility';
 import softDelete from 'objection-soft-delete';
 import moment from 'moment';
@@ -6,20 +6,28 @@ import moment from 'moment';
 const visibility = visibilityPlugin.default;
 
 const soft = softDelete({
-    columnName: 'deleted_at',
-    deletedValue: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
-    notDeletedValue: null
+	columnName: 'deleted_at',
+	deletedValue: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
+	notDeletedValue: null
 });
 
 // Extend the QueryBuilder prototype with the mergeContext function
 QueryBuilder.prototype.mergeContext = QueryBuilder.prototype.context;
 
 export default class BaseModel extends visibility(soft(Model)) {
-    $beforeInsert() {
-        this.created_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
-    }
+	$beforeInsert() {
+		this.created_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+	}
 
-    $beforeUpdate() {
-        this.updated_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
-    }
+	$beforeUpdate() {
+		this.updated_at = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+	}
+
+	static get modifiers() {
+		return {
+			selectId(builder) {
+				builder.select('id');
+			}
+		};
+	}
 }
