9e5e1f41fd388dc997c32d75b8c6f995385fd7b2
Medhat Usama <medhatusama@gmail.com>
2023-07-02 08:24:33 +0800

    chore: added bearer token support for openapi3 doc

--- config/swagger.mjs
+++ config/swagger.mjs
@@ -1,32 +1,42 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'MindIndex API',
-        version: '0.1.0',
-        description:
-            'MindIndex Api Documentation',
-        license: {
-            name: 'Licensed Under Apache license 2.0  ',
-            url: 'www.apache.org/licenses/LICENSE-2.0.html',
-        }
-    },
-    servers: [
-        {
-            url: process.env.NEXT_PUBLIC_BASEPATH,
-            description: 'Development server',
-        },
-        {
-            url: `localhost:${process.env.PORT}`,
-            description: 'Local server',
-        },
-    ],
+	openapi: '3.0.0',
+	info: {
+		title: 'MindIndex API',
+		version: '0.1.0',
+		description:
+			'MindIndex Api Documentation',
+		license: {
+			name: 'Licensed Under Apache license 2.0  ',
+			url: 'www.apache.org/licenses/LICENSE-2.0.html',
+		}
+	},
+	servers: [
+		{
+			url: process.env.NEXT_PUBLIC_BASEPATH,
+			description: 'Development server',
+		},
+		{
+			url: `localhost:${process.env.PORT}`,
+			description: 'Local server',
+		},
+
+	],
+	components: {
+		securitySchemes: {
+			bearerAuth: {
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT',
+			},
+		},
+	}
 };
 
 const options = {
-    swaggerDefinition,
-    apis: ['./routes/*.mjs', './controllers/*.mjs', './middlewares/*.mjs'],
+	swaggerDefinition,
+	apis: ['./routes/*.mjs', './controllers/*.mjs', './middlewares/*.mjs'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
