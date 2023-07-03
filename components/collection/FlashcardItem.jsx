af57f6c8f8b270179fff0db919839dedaf8d27ee
Medhat Usama <medhatusama@gmail.com>
2023-07-03 11:24:14 +0800

    feat: added flashcard listing component

--- components/collection/FlashcardItem.jsx
+++ components/collection/FlashcardItem.jsx
@@ -2,7 +2,7 @@
 import styles from '../styles/Studyset.module.css';
 import { useState } from 'react';
 
-export default function FlashcardItem({ cardIndex, card, clickHandler, setFlipped, setFlipStyle, flipped, flipStyle }) {
+export default function FlashcardItem({ card, clickHandler, setFlipped, setFlipStyle, flipped, flipStyle }) {
     const rotation = flipped ? 180 : 0;
     const frontStyle = { ...flipStyle, transform: `rotateY(${rotation}deg)` };
     const backStyle = { ...flipStyle, transform: `rotateY(${180 + rotation}deg)` };
@@ -11,11 +11,11 @@ export default function FlashcardItem({ cardIndex, card, clickHandler, setFlippe
         <div className={styles['flashcardItem']} onClick={clickHandler}>
             <div className={styles['flashcardItemInner']} style={frontStyle}>
                 <div className={styles['flashcardItemHelper']}>front</div>
-                {card.question}
+                {card?.question}
             </div>
             <div className={styles['flashcardItemInner']} style={backStyle}>
                 <div className={styles['flashcardItemHelper']}>back</div>
-                {card.answer}
+                {card?.answer}
             </div>
         </div>
     );
---
+++ components/collection/FlashcardListing.jsx
@@ -0,0 +1,31 @@
+export default function FlashcardListing({ flashcards }) {
+    return (
+        <div className="row">
+            {flashcards?.length > 0 ? (
+                flashcards.map((card, index) => (
+                    <div className="col-12" key={index}>
+                        <div className="card mb-3">
+                            <div className="row g-0">
+                                <div className="col-md-4 d-flex align-items-center justify-content-center">
+                                    <h5 className="card-title">{card?.question}</h5>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <p className="card-text">
+                                            {card?.answer}
+                                        </p>
+                                        <p className="card-text">
+                                            <small className="text-muted">{index + 1}</small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div></div>
+            )}
+        </div>
+    )
+}
