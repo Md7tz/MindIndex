5f9b10c0f8beba06f82c81b0825a1334b979e659
Medhat Usama <medhatusama@gmail.com>
2023-07-03 10:32:15 +0800

    chore: alter migration adding slug column to 'collections' table

---
+++ migrations/20230703020303_alter_collections_table_add_slug.js
@@ -0,0 +1,39 @@
+const slugify = require("slugify");
+
+exports.up = function (knex) {
+  return knex.schema
+    .alterTable("collections", function (table) {
+      table.string("slug").unique().nullable();
+    })
+    .then(() => {
+      // Update the "slug" column based on the existing "name" values
+      return knex("collections").select("id", "name").then((rows) => {
+        const updatePromises = rows.map((row) => {
+          const slug = slugify(row.name, { lower: true }); // Generate slug using slugify library
+          return knex("collections")
+            .where("id", row.id)
+            .update({ slug });
+        });
+        return Promise.all(updatePromises);
+      });
+    })
+    .then(() => {
+      // Alter the "slug" column to be not nullable
+      return knex.schema.alterTable("collections", function (table) {
+        table.string("slug").notNullable().alter();
+      });
+    })
+    .then(() => {
+      console.log("Column 'slug' has been added to the 'collections' table, populated, and made not nullable!");
+    })
+    .catch((error) => {
+      console.error(`Error adding column, populating data, and altering column: ${error}`);
+      throw error;
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable("collections", function (table) {
+    table.dropColumn("slug");
+  });
+};
