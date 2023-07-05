0c125e69b50758f467ff968a85cad0fbb2da4349
Medhat Usama <medhatusama@gmail.com>
2023-07-06 03:53:24 +0800

    fix: search card view action

--- components/search/Card.jsx
+++ components/search/Card.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import NoteForm from "../NoteForm";
+import { Navigate } from "../Basepath";
 const Card = ({ result }) => {
   return (
     <div className="card bg-dark text-white h-100">
+      {result.body && <NoteForm mode={"view"} result={result} />}
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">
           {result.title && result.title.length > 30
@@ -16,13 +18,26 @@ const Card = ({ result }) => {
         </p>
         <div className="mt-auto d-flex justify-content-end">
           {/* either for collection view or note */}
-          <a
-            href={result.title ? `#view${result.id}` : "#"}
-            className="btn btn-primary"
-            data-bs-toggle="modal"
-          >
-            View
-          </a>
+          {
+            result.body ? (
+              <a
+                href={result.title ? `#view${result.id}` : "#"}
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+              >
+                View
+              </a>) : (
+              <a
+                href={result.title ? `#view${result.id}` : "#"}
+                className="btn btn-primary"
+                onClick={
+                  () => Navigate.push(`/study-set/${result.slug}`)
+                }
+              >
+                View
+              </a>)
+          }
+
         </div>
       </div>
       {result.title && <NoteForm mode={"view"} result={result} />}
